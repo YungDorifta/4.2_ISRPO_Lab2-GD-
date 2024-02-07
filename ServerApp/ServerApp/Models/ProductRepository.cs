1a7156306f09d916d0f3eb5e2bc98c78c33140a2
unknown <429281-4>
2024-02-07 10:31:10 +0300

    Word status updated

--- ServerApp/ServerApp/Models/ProductRepository.cs
+++ ServerApp/ServerApp/Models/ProductRepository.cs
@@ -7,7 +7,7 @@ namespace ServerApp.Models
 {
     public class ProductRepository : IProductRepository
     {
-        private List<Product> products = new List<Product>();
+        private List<Product> Readers = new List<Product>();
         private int _nextId = 1;
 
         public ProductRepository()
@@ -21,34 +21,34 @@ namespace ServerApp.Models
             if (item == null)
                 throw new ArgumentNullException();
             item.Id = _nextId++;
-            products.Add(item);
+            Readers.Add(item);
             return item;
         }
 
         public Product Get(int id)
         {
-            return products.Find(p => p.Id == id);
+            return Readers.Find(p => p.Id == id);
         }
 
         public IEnumerable<Product> GetAll()
         {
-            return products;
+            return Readers;
         }
 
         public void Remove(int id)
         {
-            products.RemoveAll(p => p.Id == id);
+            Readers.RemoveAll(p => p.Id == id);
         }
 
         public bool Update(Product item)
         {
             if (item == null)
                 throw new ArgumentNullException();
-            int index = products.FindIndex(p => p.Id == item.Id);
+            int index = Readers.FindIndex(p => p.Id == item.Id);
             if (index == -1)
                 return false;
-            products.RemoveAt(index);
-            products.Add(item);
+            Readers.RemoveAt(index);
+            Readers.Add(item);
             return true;
         }
     }
