86a6896f587c0bda1d0c99b63920054d14cfdcad
unknown <429281-4>
2024-02-22 10:08:06 +0300

    Redo tables and edit functions

--- WpfApp/WpfApp/MainWindow.xaml.cs
+++ WpfApp/WpfApp/MainWindow.xaml.cs
@@ -32,6 +32,32 @@ namespace WpfApp
         public MainWindow()
         {
             InitializeComponent();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:50237/api/");
+
+                //HTTP GET
+                var responseTask = client.GetAsync("products");
+                responseTask.Wait();
+
+                var GetResult = responseTask.Result;
+                if (GetResult.IsSuccessStatusCode)
+                {
+
+                    var readTask = GetResult.Content.ReadAsAsync<Product[]>();
+                    readTask.Wait();
+
+                    var products = readTask.Result;
+
+                    foreach (var product in products)
+                    {
+                        Console.WriteLine("{0} {1} {2} {3}", product.Id, product.Name,
+                        product.Category, product.Price);
+                    }
+                }
+            }
+
         }
     }
 }
